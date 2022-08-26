a5576d8181ccf5ae04319ff17f2d2236310d3c97
Daniel Gelsdorf <afonsodaniel985@gmail.com>
2022-08-25 21:21:02 -0300

    Projeto com função

--- Program.cs
+++ Program.cs
@@ -1,49 +1,51 @@
-﻿string i="a";
-float dia = 20;
-var total = 30.0;
-float hora;
-float ano=12;
+﻿float dia (float salario){
+    float dia=30;
+    return salario/dia;
+}
+float hora(float salario){
+    float dia=30;
+    float hora =8;
+    return salario/dia/hora;
+}
+float mes(float salario){
+    return salario;
+}
+float ano(float salario){
+    float ano=12;
+    return salario*ano;
+}
+int salario;
+string i="a";
 string nome;
-float salario;
-Console.WriteLine("digite seu nome");
-nome =Console.ReadLine();
-Console.WriteLine($"bem vindo: {nome}\n", nome);
+Console.WriteLine("digite seu nome!");
+nome=Convert.ToString(Console.ReadLine());
+Console.WriteLine($"bem vindo{nome}",nome);
 Console.WriteLine("digite seu salário");
-salario = Convert.ToInt32(Console.ReadLine());
-while( i !="s"){
-    Console.WriteLine("escolha sua opção");
-    Console.WriteLine("Digite A quanto ganho por hora\n");
-    Console.WriteLine("Digite D para ver quanto ganho por cemana.\n");
-    Console.WriteLine("digite C para ver quanto ganha no mes\n");
-    Console.WriteLine("digite D para ver quanto ganha no dia\n");
-    Console.WriteLine("digite E para ver quanto ganha no ano\n");
-    Console.WriteLine("Digite S para sair");
-    switch(i=Console.ReadLine())
-    {
-        case "a":
-            hora = salario / 20 / 8;
-            Console.WriteLine($"ganha por hora{hora}", hora);
+salario=Convert.ToInt32(Console.ReadLine());
+while(i!="s"){
+    Console.WriteLine("digite a para ver quanto ganha por dia\n b para ver quanto ganha por hora\n c para ver quanto ganha no mês\n d quanto ganha no ano \n e s para sair.");
+    switch(i=Convert.ToString(Console.ReadLine())){
+        case"a":
+            float mdia=dia(salario);
+            Console.WriteLine($"ganha no dia{mdia}",mdia);
             break;
         case "b":
-            total = salario / 4;
-            Console.WriteLine($"seu salário na semana é: {total}", total);
+            float mhora=hora(salario);
+            Console.WriteLine($"ganha por hora{mhora}",mhora);
             break;
         case "c":
-            Console.WriteLine($"ganhos mensais:{salario}", salario);
+            float mmes=mes(salario);
+            Console.WriteLine($"salário mensal{mmes}",mmes);
             break;
         case "d":
-            dia = (salario / dia);
-            Console.WriteLine($"salario  diário:{dia}", dia);
+            float mano=ano(salario);
+            Console.WriteLine($"seu salário anual é:{mano}",mano);
             break;
-        case "e":
-            ano = salario * ano;
-            Console.WriteLine($"salario anual: {ano}", ano);
-            break;
-        case "s":
+        case"s":
             Console.WriteLine("obrigado por usar o programa.");
             break;
         default:
-            Console.WriteLine("opção errada, por favor digite a opção correta.");
+            Console.WriteLine("opção errada, por favor digite outra!");
             break;
     }
 }
